38faf34752f38a14c4acd6150ad19fcf564dfbbd
Max Gorce <max.gorce@yahoo.com>
2024-04-08 21:35:51 +0200

    mise a jour de la demande client

--- src/main/java/canard/model/PrototypeCanard.java
+++ src/main/java/canard/model/PrototypeCanard.java
@@ -1,31 +1,24 @@
 package canard.model;
 
 import canard.model.cancan.Cancan;
-import canard.model.vol.ComportementVol;
 import canard.model.vol.NePasVoler;
-import canard.model.vol.PropulsionAReaction;
 
 public class PrototypeCanard extends Canard {
 
-    private ComportementVol comportementVol;
-
-    public PrototypeCanard(String nom, boolean vole) {
-        super(nom, vole ? new PropulsionAReaction() : new NePasVoler(), new Cancan());
-        this.comportementVol = vole ? new PropulsionAReaction() : new NePasVoler();
+	public PrototypeCanard(String nom) {
+        super(nom, new NePasVoler(), new Cancan());
     }
 
     @Override
     public String nager() {
-        return null;
+        return "Tous les canards flottent, même les leurres!";
     }
+
 
     @Override
     public String afficher() {
         return "Je suis un prototype de canard";
     }
 
-    public ComportementVol getComportementVol() {
-        return comportementVol;
-    }
-
 }
+
